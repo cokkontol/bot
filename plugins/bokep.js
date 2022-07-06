ac818b737eb55297a22be9dfb15c89ff891abf7f
Ajitegar <108433103+Ajitegar@users.noreply.github.com>
2022-07-06 13:26:42 +0700

    Update bokep.js

--- plugins/bokep.js
+++ plugins/bokep.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
  conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'Nih kak jngan lupa follow ig owner', m)
 }
 handler.help = ['bokep']
-handler.tags = ['fun']
+handler.tags = ['asupan']
 
 handler.command = /^(bokep)$/i
 handler.premium = true
