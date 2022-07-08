4dc4aaab7efdfed9d5fceed5ce01d8992e76e950
pinocopas <108897189+pinocopas@users.noreply.github.com>
2022-07-08 13:44:50 +0700

    Update sertifikat.js

--- plugins/sertifikat.js
+++ plugins/sertifikat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, command }) => {
     if (!text) throw 'Masukkan Teks.....................'
     let hyz = `https://api.lolhuman.xyz/api/${command}?apikey=PapahChan&name=${text}`
-    conn.sendButtonImg(m.chat, hyz, wm, m) 
+    conn.sendFile(m.chat, hyz, wm, m) 
 }
 handler.help = ['toloserti', 'fuckboy', 'fuckgirl', 'bucinserti', 'goodboy', 'goodgirl', 'badboy', 'badgirl'].map(v => v + ' <teks>')
 handler.tags = ['maker']
