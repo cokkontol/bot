38458a1e23d8528de85c469fddde5d29aa848f5b
pinocopas <108897189+pinocopas@users.noreply.github.com>
2022-07-11 04:41:03 +0700

    Update ceritahoror.js

--- plugins/ceritahoror.js
+++ plugins/ceritahoror.js
@@ -1,6 +1,6 @@
-let axios = require('axios')
+let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let url = await axios('https://api.lolhuman.xyz/api/ceritahoror?apikey=86ca9a668a002cb297236c64')
+  let url = await fetch('https://api.lolhuman.xyz/api/ceritahoror?apikey=86ca9a668a002cb297236c64')
   let cerhan = await url.json()
 let hasil = `
 *Powered By ${global.wm}*
