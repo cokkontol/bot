40df2a53056d044f3fadaf14b362f7ab1c92f91e
pinocopas <108897189+pinocopas@users.noreply.github.com>
2022-07-11 04:21:36 +0700

    Update randomimage.js

--- plugins/randomimage.js
+++ plugins/randomimage.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command }) => {
 let nyenye = `https://api-reysekha.herokuapp.com/api/wallpaper/${command}?apikey=APIKEY`
-    conn.File(m.chat, nyenye, 'Nih', `.${command}`) 
+    conn.sendFile(m.chat, nyenye, 'Nih', `.${command}`) 
 }
 handler.help = ['wallhp', 'kartun', 'katakata', 'hekel', 'anime', 'jeni']
 handler.tags = ['internet']
