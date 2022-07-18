3908eb4c9b9eb0c3c157085cfca07928c6434789
copasxrename <109092712+copasxrename@users.noreply.github.com>
2022-07-18 19:59:02 +0700

    Update ceksifat.js

--- plugins/ceksifat.js
+++ plugins/ceksifat.js
@@ -21,7 +21,7 @@ handler.help = ['ceksifat <nama>']
 handler.tags = ['fun']
 handler.command = /^ceksifat/i
 
-export default handler 
+default.export handler 
 
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
