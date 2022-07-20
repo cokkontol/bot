763888ef14c8dbd9f6ee3cd03c127bc6ae042930
Pino <109092712+hy-pin@users.noreply.github.com>
2022-07-21 04:11:39 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -17,7 +17,7 @@ handler.command = /^(tik(tok)?(dl)?)$/i
 module.exports = handler*/
 
 
-const hxz = require("hxz-api")
+/*const hxz = require("hxz-api")
 let handler = async(m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
 if (!args[0].match(/tiktok/gi)) throw `*Link salah! Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
@@ -31,25 +31,21 @@ handler.tags = ['downloader']
 handler.command = /^(tiktok|tiktokdl)$/i
 handler.limit = true
 handler.group = true
-module.exports = handler
+module.exports = handler*/
 
-/*
-const { tiktokdl, tiktokdlv2 } = require('@bochilteam/scraper')
+
+let axios = require('axios');
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
-    if (!args[0].match(/tiktok/gi)) throw `*Link salah! Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
-    const url = video.no_watermark || video.no_watermark_hd || video.with_watermark || video.no_watermark_raw
-    if (!url) throw 'Can\'t download video!'
-    m.reply('Sedang diproses...')
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `*© Aine*
-`.trim(), m)
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    conn.sendFile(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', m)
 }
-handler.help = ['tiktok <url>']
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-
-handler.command = /^(tik|tt|tiktok)$/i
+handler.command = /^(tik(tok)?(dl)?)$/i
+handler.limit = 1
 
 module.exports = handler
-*/
 
