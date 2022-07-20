d2aeddbf1efaef5bf973696ffe057fc93d559687
Pino <109092712+hy-pin@users.noreply.github.com>
2022-07-21 04:58:03 +0700

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  conn.sendFile(m.chat, global.API('lol', '/api/attp', { apikey: 'pkebgk8248jskrkfm', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
+  conn.sendFile(m.chat, global.API('lol', '/api/attp', { apikey: '9b817532fadff8fc7cb86862', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
