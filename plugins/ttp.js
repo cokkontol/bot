fa13fc70cb76f7f738a9cfff491c3219ee23d6af
Pino <109092712+hy-pin@users.noreply.github.com>
2022-07-21 04:57:16 +0700

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -3,7 +3,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  let stiker = await sticker(null, global.API('lol', '/api/ttp', { apikey: 'pkebgk8248jskrkfm', text: teks }), global.packname, global.author)
+  let stiker = await sticker(null, global.API('lol', '/api/ttp', { apikey: '9b817532fadff8fc7cb86862', text: teks }), global.packname, global.author)
   if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, { asSticker: true })
   throw stiker.toString()
 }
