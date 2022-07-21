a409261833117dbd7e9a2a9703e0deb1973b6077
Pino <109092712+hy-pin@users.noreply.github.com>
2022-07-22 03:49:37 +0700

    Update mediafire.js

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -1,20 +1,41 @@
-const { mediafireDl } = require('../lib/mediafire.js')
-let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-if (!text) return m.reply(`Kirim perintah ${usedPrefix + command} *link mediafire*`)
-if (!args[0].includes('mediafire.com')) return m.reply(error.linkmf)
-let mdjon = args.join(' ')
-res = await mediafireDl(mdjon)
-result = `「 *MEDIAFIRE DOWNLOAD* 」
-*Data Berhasil Didapatkan!*
-🆔 Nama : ${res[0].nama}
-📊 Ukuran : ${res[0].size}
-💬 Link : ${res[0].link}
-_Tunggu Proses Upload Media_`
-m.reply(result)
-//await sleep(100)
-conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true, mimetype:res[0].mime})
+const fetch = require('node-fetch')
+const axios = require('axios')
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys')
+const { mediafiredl } = require('@bochilteam/scraper')
+let handler = async (m, { isOwner, isPrems, command, usedPrefix, text, args, conn }) => {
+     var limit
+     if((isOwner || isPrems)) limit = 250
+     else limit = 100
+     if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
+     if (!args[0].match(/mediafire/gi)) throw `url salah`
+     const sentMsg = await m.reply('*_Tunggu Sebentar Aine Sedang Memprosesnya..._*')
+     let full = /f$/i.test(command)
+     let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
+     let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
+     let res = await mediafiredl(args[0])
+     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
+     let isLimit = (isPrems || isOwner ? limit : limit) * 1024 < filesize
+     let error = `Gagal mendownload..`
+     let capt = `📭 *Mediafire Downloader*
+
+📝 *Name:* ${filename}
+🎚 *Size:* ${filesizeH}
+🗃 *Extension:* ${ext}
+📤 *Uploaded:* ${aploud}
+${isLimit ? `❌ *Ukuran file di atas ${limit} MB, download sendiri*\n` : ''}🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${url}`)).data}` 
+     
+     if(ss) await conn.sendFile(m.chat, ss, 'screenshot.png', capt, sentMsg, 0, {jpegThumbnail: ss})
+     try {
+     if(!isLimit) await conn.sendMedia(m.chat, url, 0, {fileName: `${filename}`, mentions: [m.sender]})
+     } catch {
+      throw error
+  }
 }
-handler.command = ['mediafire']
+handler.help = ['mediafire'].map(v => v + ' <url>')
 handler.tags = ['downloader']
+handler.command = /^(me?d(ia)?f(ire)?)$/i
+
+handler.limit = true
+handler.group = true
 
 module.exports = handler
