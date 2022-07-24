49d53ce6e38ad714dcb2760e330f6a0c12f09168
Pino <109092712+hy-pin@users.noreply.github.com>
2022-07-24 19:00:36 +0700

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,52 +1,4 @@
-/*let limit = 30
-const fetch = require('node-fetch')
-// const { servers, ytv } = require('../lib/y2mate')
-const { youtubedl, youtubedlv2 } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
-  let chat = global.db.data.chats[m.chat]
-  let isY = /y(es)/gi.test(args[1])
-  const { thumbnail, video: _video, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0]))
-  let video, link = '', lastError
-  for (let i in _video) {
-    try {
-      video = _video[i]
-      link = await video.download()
-      if (link) break
-    } catch (e) {
-      lastError = e
-      continue
-    }
-  }
-  if (!link) throw lastError
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < video.fileSize
-  if (!isY) conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
-📌*Title:* ${title}
-🗎 *Filesize:* ${video.fileSizeH}
-*${isLimit ? 'Pakai ' : ''}Link:* ${link}
-`.trim(), m)
-  let _thumb = {}
-  try { _thumb = { thumbnail: await (await fetch(thumbnail)).buffer() } }
-  catch (e) { }
-  if (!isLimit) conn.sendFile(m.chat, link, title + '.mp4', `
-📌 *Title:* ${title}
-🗎 *Filesize:* ${video.fileSizeH}
-`.trim(), m, false, {
-    ..._thumb,
-    asDocument: chat.useDocument
-  })
-}
-handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> <without message>`)
-handler.tags = ['downloader']
-handler.command = /^yt(v|mp4)?$/i
-
-handler.exp = 0
-
-
-module.exports = handler*/
-
-
-let limit = 30
+let limit = 1024
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
@@ -55,10 +7,12 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+  let name = await conn.getName(m.sender)
+let wm = global.wm
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
 *Title:* ${title}
 *Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
+*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
 `.trim(), m)
   let _thumb = {}
   try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
@@ -71,13 +25,13 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   asDocument: chat.useDocument
 })
 }
-handler.help = ['mp4','v',''].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
+handler.help = ['mp4','v',''].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = true
+handler.group = false
 handler.private = false
 
 handler.admin = false
@@ -88,8 +42,3 @@ handler.exp = 0
 handler.limit = true
 
 module.exports = handler
-
-async function shortlink(url) {
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
-}
